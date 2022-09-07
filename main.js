9a5d78b7659508e4a2d58cf799cbe33fcc7cb636
Hsiwozer <1377311206@qq.com>
2022-09-07 23:45:46 +0800

    commit home

--- main.js
+++ main.js
@@ -2,6 +2,37 @@ import App from './App'
 
 // #ifndef VUE3
 import Vue from 'vue'
+
+// 按需导入 $http 对象
+import { $http } from '@escook/request-miniprogram'
+
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+
+// 请求根路径
+$http.baseUrl = 'https://api-ugo-web.itheima.net'
+
+// 请求拦截器
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...'
+  })
+}
+
+// 响应拦截器
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+
+// 封装弹窗方法
+uni.$showMsg = function(title = '数据请求失败!', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none'
+  })
+}
+
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
